9f7ed58aeaebf4f788a7a10fc65a449668090ef6
Lauri Jakku <lja@iki.fi>
2020-04-03 14:12:17 +0300

    example as base

--- app/src/main/java/com/lja/bluecorona/cBTRFCommConstants.java
+++ app/src/main/java/com/lja/bluecorona/cBTRFCommConstants.java
@@ -1,16 +1,32 @@
 package com.lja.bluecorona;
 
+import java.util.HashMap;
+import java.util.HashMap;
 import java.util.UUID;
 
-public interface cBTRFCommConstants {
-    public static final String cBTRFCommUIDString =
-                new String("BluetoothRFCommCoronaAlarm");
-    public static final UUID cBTRFCommUID =
-                UUID.fromString(cBTRFCommUIDString);
+public class cBTRFCommConstants {
 
     public static final int cBTRFCommUserNotSet  = 0;
     public static final int cBTRFCommUserSick    = 1;
     public static final int cBTRFCommUserOK      = 2;
     public static final int cBTRFCommUserNotSure = 3;
+
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    static {
+        // Sample Services.
+        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        // Sample Characteristics.
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
 
 }
